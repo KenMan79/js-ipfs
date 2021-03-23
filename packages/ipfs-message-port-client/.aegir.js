86d977589253fe51c0cb52c3e305a4994919de2b
achingbrain <alex@achingbrain.net>
2021-03-23 06:25:36 +0000

    chore: bundle size

--- packages/ipfs-message-port-client/.aegir.js
+++ packages/ipfs-message-port-client/.aegir.js
@@ -21,7 +21,7 @@ const buildConfig = {
 /** @type {import('aegir').PartialOptions} */
 module.exports = {
   build: {
-    bundlesizeMax: '40kB',
+    bundlesizeMax: '46kB',
     config: buildConfig
   },
   test: {
